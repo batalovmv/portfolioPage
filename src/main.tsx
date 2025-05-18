d115145419fa99e3fb4208dafc31133aa95d9c06
batalovmv <55100840+batalovmv@users.noreply.github.com>
2025-05-18 15:50:12 +0300

    исправл проблема поломки при обновлении сайта

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 import App from './App.tsx'
 import "./styles/main.css";
 import store from './store';
@@ -10,9 +10,9 @@ import reportWebVitals from './reportWebVitals.ts';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/portfolioPage">
+      <HashRouter  basename="/portfolioPage">
         <App />
-      </BrowserRouter>
+      </HashRouter >
     </Provider>
   </React.StrictMode>,
 )
