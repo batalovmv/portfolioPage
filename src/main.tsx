ea67f1016c39aa20f4cc117b5682a04747c15bbd
batalovmv <55100840+batalovmv@users.noreply.github.com>
2025-05-18 18:16:32 +0300

    вернул browserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux';
-import { HashRouter} from 'react-router-dom';
+import { BrowserRouter, HashRouter} from 'react-router-dom';
 import App from './App.tsx'
 import "./styles/main.css";
 import store from './store';
@@ -10,9 +10,9 @@ import reportWebVitals from './reportWebVitals.ts';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter  basename="/portfolioPage">
+      <BrowserRouter  basename="/portfolioPage">
         <App />
-      </HashRouter >
+      </BrowserRouter >
     </Provider>
   </React.StrictMode>,
 )
